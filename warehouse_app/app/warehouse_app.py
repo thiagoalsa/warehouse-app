92430e186c16b2f6257987f1e4aa7bac945b5c12
Thiago de Almeida Santos <thiago.as1994@gmail.com>
2023-04-25 21:39:59 +0100

    Create Dialog

--- warehouse_app/app/warehouse_app.py
+++ warehouse_app/app/warehouse_app.py
@@ -3,6 +3,8 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivymd.uix.button import MDFloatingActionButton
+from kivymd.uix.dialog import MDDialog
+from kivy.uix.boxlayout import BoxLayout
 
 
 # Create a Pandas DataFrame
@@ -19,6 +21,11 @@ style['link'] = images['link']
 class MainScreen(Screen):
     ...
 
+# Create a Dialog class
+
+class SearchBox(BoxLayout):
+    ...
+
 # Create class MainApp 
 
 class MainApp(MDApp):
@@ -28,6 +35,13 @@ class MainApp(MDApp):
     
     def callback(self, instance_action_top_appbar_button):
         print(instance_action_top_appbar_button)
+
+
+    dialog_search = None
+    def search(self):
+        if not self.dialog_search:
+            self.dialog_search = MDDialog(type='custom', content_cls=SearchBox())
+        self.dialog_search.open()
 
 
 # Run the app
